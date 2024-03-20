0a7ce812bebb3b21e124ca9b0e4ddafaf93b341a
AlianBlank <wangfj11@foxmail.com>
2024-03-20 19:16:47 +0800

    [增加]1. 增加协议的获取

--- src/gameframex/proto/ProtoMessageHelper.ts
+++ src/gameframex/proto/ProtoMessageHelper.ts
@@ -1,3 +1,10 @@
 export default class ProtoMessageHelper {
+    private static protobuf: any = (window as any).protobuf.roots.default;
+    public static getMessageTypeByModule(moduleName: string, typeName: string): any {
+        return this.protobuf[moduleName][typeName];
+    }
 
+    public static getMessageType(typeName: string): any {
+        return this.protobuf[typeName];
+    }
 }
