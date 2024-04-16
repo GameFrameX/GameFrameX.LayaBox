afaf609a232b242eacf7d75898b6063d3743de8c
AlianBlank <wangfj11@foxmail.com>
2024-04-16 18:49:48 +0800

    [增加]1. 增加协议相关的加载路径

---
+++ src/gameframex/utility/UtilityAsset.ts
@@ -0,0 +1,5 @@
+export default class UtilityAsset {
+    public static getProtobuffList(): string[] {
+        return ["resources/protobuf/Basic.proto", "resources/protobuf/Bag.proto", "resources/protobuf/User.proto", "resources/protobuf/Common.proto"];
+    }
+}
