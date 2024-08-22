8c11d55737b32fd9839e7175db29cf043f62dd3c
Blank <wangfj11@foxmail.com>
2024-08-22 20:57:43 +0800

    [删除]1. 删除废弃文件

--- src/gameframex/utility/UtilityAsset.ts
+++
@@ -1,5 +0,0 @@
-export default class UtilityAsset {
-    public static getProtobuffList(): string[] {
-        return ["resources/protobuf/Basic.proto", "resources/protobuf/Bag.proto", "resources/protobuf/User.proto", "resources/protobuf/Common.proto"];
-    }
-}
